13edbdafa262a236edab9c22652bf87bd3dda265
Johan van Graan <johan@jvg.co.za>
2024-04-20 19:00:09 +0200

    Display Critic Score.

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,25 @@
+/** @format */
+
+import { Badge } from "@chakra-ui/react";
+
+export interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  let color = score > 75 ? "green" : score > 60 ? "yellow" : "";
+  return (
+    <>
+      <Badge
+        colorScheme={color}
+        fontSize='14px'
+        paddingX={2}
+        borderRadius='4px'
+      >
+        {score}
+      </Badge>
+    </>
+  );
+};
+
+export default CriticScore;
