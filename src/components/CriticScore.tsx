27a0ab22b5c0272861c47fe2bda0b32b78dffa03
Johan van Graan <johan@jvg.co.za>
2024-04-21 07:55:06 +0200

    Get optimized images

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -7,12 +7,12 @@ export interface Props {
 }
 
 const CriticScore = ({ score }: Props) => {
-  let color = score > 75 ? "green" : score > 60 ? "yellow" : "";
+  const color = score > 75 ? "green" : score > 60 ? "yellow" : "";
   return (
     <>
       <Badge
         colorScheme={color}
-        fontSize='14px'
+        fontSize='16px'
         paddingX={2}
         borderRadius='4px'
       >
