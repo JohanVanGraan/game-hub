574524994ef195ea1fae39de46e00d48b6e3f3e7
Johan van Graan <johan@jvg.co.za>
2024-04-22 10:48:17 +0200

    Display Genres

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,15 @@ function App() {
           base: `"nav" "main"`,
           lg: `"nav nav" "aside main"`, // 1024 wide
         }}
+        templateColumns={{ base: "ifr", lg: "200px 1fr" }}
       >
         <GridItem area='nav'>
           <NavBar />
         </GridItem>
         <Show above='lg'>
-          <GridItem area='aside'>{<GenreList />}</GridItem>
+          <GridItem area='aside' paddingX={5}>
+            {<GenreList />}
+          </GridItem>
         </Show>
         <GridItem area='main'>
           <GameGrid />
